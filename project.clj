8a6b9c0fb2cc864d37d6b98856edbfa0dee3e4dd
Chad Harrington <chad@deercreeklabs.com>
2017-11-02 17:37:17 -0600

    Dry up project.clj.

--- project.clj
+++ project.clj
@@ -1,3 +1,47 @@
+(def externs ["lancaster_externs.js"])
+
+(def compiler-defaults
+  {:npm-deps {:avsc "5.0.5"}
+   :install-deps true
+   :parallel-build true
+   :static-fns true
+   ;;:pseudo-names true
+   ;;:pretty-print true
+   ;;:infer-externs true
+   })
+
+(defn make-build-conf [id target-kw build-type-kw opt-level main]
+  (let [build-type-str (name build-type-kw)
+        target-str (if target-kw
+                     (name target-kw)
+                     "")
+        node? (= :node target-kw)
+        source-paths (case build-type-kw
+                       :build ["src"]
+                       :test ["src" "test"])
+        build-name (str target-str "_" build-type-str "_" (name opt-level))
+        output-name (case build-type-kw
+                      :build "main.js"
+                      :test "test_main.js")
+        output-dir (str "target/" build-type-str "/" build-name)
+        output-to (str output-dir "/" output-name)
+        source-map (if (= :none opt-level)
+                     true
+                     (str output-dir "/map.js.map"))
+        compiler (cond-> compiler-defaults
+                   true (assoc :optimizations opt-level
+                               :output-to output-to
+                               :output-dir output-dir
+                               :source-map source-map)
+                   main (assoc :main main)
+                   (= :advanced opt-level) (assoc :externs externs)
+                   node? (assoc :target :nodejs))
+        node-test? (and node? (= :test build-type-kw))]
+    (cond-> {:id id
+             :source-paths source-paths
+             :compiler compiler}
+      node-test? (assoc :notify-command ["node" output-to]))))
+
 (defproject deercreeklabs/lancaster "0.1.5-SNAPSHOT"
   :description "Tools for working with Apache Avro"
   :url "http://www.deercreeklabs.com"
@@ -26,8 +70,7 @@
     [[doo "0.1.8"]
      [org.clojure/tools.namespace "0.2.11"]]}}
 
-  :npm {:dependencies [[avsc "5.0.5"]]
-        :devDependencies [[karma "1.7.1"]
+  :npm {:devDependencies [[karma "1.7.1"]
                           [karma-chrome-launcher "2.2.0"]
                           [karma-cljs-test "0.1.0"]
                           [karma-firefox-launcher "1.0.1"]
@@ -55,100 +98,30 @@
 
   :cljsbuild
   {:builds
-   [{:id "node-test-none"
-     :source-paths ["src" "test"]
-     :notify-command ["node" "target/test/node_test_none/test_main.js"]
-     :compiler
-     {:optimizations :none
-      :parallel-build true
-      :main "deercreeklabs.node-test-runner"
-      :target :nodejs
-      :output-to "target/test/node_test_none/test_main.js"
-      :output-dir "target/test/node_test_none"
-      :source-map true}}
-    {:id "node-test-adv"
-     :source-paths ["src" "test"]
-     :notify-command ["node" "target/test/node_test_adv/test_main.js"]
-     :compiler
-     {:optimizations :advanced
-      ;;:pseudo-names true
-      ;;:pretty-print true
-      ;;:infer-externs true
-      :externs ["lancaster_externs.js"]
-      :parallel-build true
-      :main "deercreeklabs.node-test-runner"
-      :target :nodejs
-      :static-fns true
-      :output-to  "target/test/node_test_adv/test_main.js"
-      :output-dir "target/test/node_test_adv"
-      :source-map "target/test/node_test_adv/map.js.map"}}
-    {:id "node-test-simple"
-     :source-paths ["src" "test"]
-     :notify-command ["node" "target/test/node_test_simple/test_main.js"]
-     :compiler
-     {:optimizations :simple
-      :parallel-build true
-      :main "deercreeklabs.node-test-runner"
-      :target :nodejs
-      :static-fns true
-      :output-to  "target/test/node_test_simple/test_main.js"
-      :output-dir "target/test/node_test_simple"
-      :source-map "target/test/node_test_simple/map.js.map"}}
-    {:id "doo-test-none"
-     :source-paths ["src" "test"]
-     :compiler
-     {:optimizations :none
-      :parallel-build true
-      :main "deercreeklabs.doo-test-runner"
-      :output-to "target/test/doo_test_none/test_main.js"
-      :output-dir "target/test/doo_test_none"
-      :source-map true}}
-    {:id "doo-test-simple"
-     :source-paths ["src" "test"]
-     :compiler
-     {:optimizations :simple
-      :parallel-build true
-      :main "deercreeklabs.doo-test-runner"
-      :output-to "target/test/doo_test_simple/test_main.js"
-      :output-dir "target/test/doo_test_simple"
-      :source-map "target/test/doo_test_simple/map.js.map"}}
-    {:id "doo-test-adv"
-     :source-paths ["src" "test"]
-     :compiler
-     {:optimizations :advanced
-      ;; :pseudo-names true
-      ;; :pretty-print true
-      ;; :infer-externs true
-      :externs ["lancaster_externs.js"]
-      :parallel-build true
-      :main "deercreeklabs.doo-test-runner"
-      :static-fns true
-      :output-to  "target/test/doo_test_adv/test_main.js"
-      :output-dir "target/test/doo_test_adv"
-      :source-map "target/test/doo_test_adv/map.js.map"}}
-    {:id "build-simple"
-     :source-paths ["src"]
-     :compiler
-     {:optimizations :simple
-      :parallel-build true
-      :static-fns true
-      :output-to  "target/build_simple/tube.js"
-      :output-dir "target/build_simple"
-      :source-map "target/build_simple/map.js.map"}}]}
+   [~(make-build-conf "node-test-none" :node :test :none
+                      "deercreeklabs.node-test-runner")
+    ~(make-build-conf "node-test-simple" :node :test :simple
+                      "deercreeklabs.node-test-runner")
+    ~(make-build-conf "node-test-adv" :node :test :advanced
+                      "deercreeklabs.node-test-runner")
+    ~(make-build-conf "doo-test-none" :doo :test :none
+                      "deercreeklabs.doo-test-runner")
+    ~(make-build-conf "doo-test-simple" :doo :test :simple
+                      "deercreeklabs.doo-test-runner")
+    ~(make-build-conf "doo-test-adv" :doo :test :advanced
+                      "deercreeklabs.doo-test-runner")
+    ~(make-build-conf "build-adv" nil :build :advanced nil)]}
 
   :aliases
   {"auto-test-cljs" ["do"
                      "clean,"
                      "cljsbuild" "auto" "node-test-none"]
-   "auto-test-cljs-adv" ["do"
-                         "clean,"
-                         "cljsbuild" "auto" "node-test-adv"]
    "auto-test-cljs-simple" ["do"
                             "clean,"
                             "cljsbuild" "auto" "node-test-simple"]
-   "build-simple" ["do"
-                   "clean,"
-                   "cljsbuild" "once" "build-simple"]
+   "auto-test-cljs-adv" ["do"
+                         "clean,"
+                         "cljsbuild" "auto" "node-test-adv"]
    "chrome-test" ["do"
                   "clean,"
                   "doo" "chrome" "doo-test-adv"]})
