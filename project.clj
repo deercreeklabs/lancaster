bbb842934da2adb1e0730aa3e2bf0c6a465a4a0f
Chad Harrington <chad@deercreeklabs.com>
2017-12-09 23:49:47 -0700

    lein-release plugin: bumped version from 0.1.10 to 0.1.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/lancaster "0.1.10"
+(defproject deercreeklabs/lancaster "0.1.11-SNAPSHOT"
   :description "Tools for working with Apache Avro"
   :url "https://github.com/deercreeklabs/lancaster"
   :license {:name "Apache License, Version 2.0"
