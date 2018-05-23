6bf3a6b36c4a1404b04b02661644b5ee5cac2cb3
Chad Harrington <chad@deercreeklabs.com>
2018-05-23 16:32:40 -0600

    lein-release plugin: preparing 0.1.30 release

--- project.clj
+++ project.clj
@@ -42,7 +42,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/lancaster "0.1.30-SNAPSHOT"
+(defproject deercreeklabs/lancaster "0.1.30"
   :description "Tools for working with Apache Avro"
   :url "https://github.com/deercreeklabs/lancaster"
   :license {:name "Apache License, Version 2.0"
