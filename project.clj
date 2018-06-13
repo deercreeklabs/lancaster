e87a72ebc5e6b91d5209a84696efc35eca6558af
Chad Harrington <chad@deercreeklabs.com>
2018-06-13 13:23:18 -0600

    Upgrade deps

--- project.clj
+++ project.clj
@@ -83,7 +83,7 @@
    [cheshire "5.8.0"]
    [com.taoensso/timbre "4.10.0"]
    [deercreeklabs/baracus "0.1.6" :exclusions [prismatic/schema]]
-   [deercreeklabs/log-utils "0.1.5"]
+   [deercreeklabs/log-utils "0.1.6"]
    [org.clojure/clojure "1.9.0"]
    [org.clojure/clojurescript "1.10.238"]
    [primitive-math "0.1.6"]
