b94ed97609b4d9c34725d115a502ed0a2477d480
Matt Burbidge <burbma@gmail.com>
2023-04-27 00:31:26 -0600

    minimal reproduction of potential bug

--- dev/user.clj
+++ dev/user.clj
@@ -1,1 +1,46 @@
-(ns user)
+(ns user
+  (:require
+   [deercreeklabs.lancaster :as l]))
+
+(def writer-edn
+  {:name :lancaster.schemas/container
+   :type :record
+   :fields [{:name :things
+             :type {:type :array
+                    :items {:name :lancaster.schemas/thing
+                            :type :record
+                            :fields []}}}
+            {:name :things-copy
+             :type {:type :array
+                    :items :lancaster.schemas/thing}}]})
+
+
+(def reader-edn
+  {:name :lancaster.schemas/container
+   :type :record
+   :fields [{:name :things
+             :type {:type :array
+                    :items {:name :lancaster.schemas/thing
+                            :type :record
+                            ; This entry in :fields is the only difference
+                            :fields [{:name :new-field
+                                      :type [:null :string]
+                                      :default nil}]}}}
+            {:name :things-copy
+             :type {:type :array
+                    :items :lancaster.schemas/thing}}]})
+
+(def value {:things [{}]
+            :things-copy [{}]})
+
+;; This works
+(true?
+  (= value
+     (l/deserialize (l/edn->schema writer-edn)
+                    (l/edn->schema writer-edn)
+                    (l/serialize (l/edn->schema writer-edn) value))))
+
+;; This throws
+(l/deserialize (l/edn->schema reader-edn)
+               (l/edn->schema writer-edn)
+               (l/serialize (l/edn->schema writer-edn) value))
